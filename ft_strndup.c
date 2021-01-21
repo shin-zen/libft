18b4d9a5e5206a6c2a41f8943bd59e9a02832df3
skodama <skodama@student.42tokyo.jp>
2021-01-22 07:16:21 +0900

    fix ft_strjoin add ft_strndup

--- ft_strndup.c
+++ ft_strndup.c
@@ -1,42 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skodama <skodama@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/28 10:55:56 by skodama           #+#    #+#             */
-/*   Updated: 2021/01/22 07:14:37 by skodama          ###   ########.fr       */
+/*   Created: 2021/01/22 07:16:11 by skodama           #+#    #+#             */
+/*   Updated: 2021/01/22 07:16:12 by skodama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strndup(const char *s1, size_t n)
 {
-	char	*p;
+	char	*copy;
 	size_t	i;
-	size_t	j;
-	size_t	len1;
-	size_t	len2;
 
 	i = 0;
-	j = 0;
-	if (!s1 && !s2)
+	if (!s1)
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	if (!(p = (char *)malloc((len1 + len2 + 1) * sizeof(char))))
+	if (!(copy = (char *)malloc((n + 1) * sizeof(char))))
 		return (NULL);
-	while (i < len1)
+	while (s1[i] && i < n)
 	{
-		p[i] = s1[i];
+		copy[i] = s1[i];
 		i++;
 	}
-	while (j < len2)
-		p[i++] = s2[j++];
-	p[i] = '\0';
-	free((char *)s1);
-	s1 = NULL;
-	return (p);
+	copy[i] = '\0';
+	return (copy);
 }
